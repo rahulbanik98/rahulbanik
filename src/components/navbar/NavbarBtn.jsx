8e43d7a02dbe0e92693fc6e2440f8c64dcc89ad1
rahulbanik98 <59626411+rahulbanik98@users.noreply.github.com>
2025-03-16 16:58:47 +0530

    add download cv

--- src/components/navbar/NavbarBtn.jsx
+++ src/components/navbar/NavbarBtn.jsx
@@ -4,27 +4,6 @@ import { LuArrowDownRight } from "react-icons/lu";
 const NavbarBtn = () => {
   return (
     <>
-      {/* <div className="flex gap-3"> */}
-      {/* <button
-          className=" px-4 py-2 
-      rounded-full text-xl 
-      font-bold font-body text-white border-cyan 
-      border flex items-center gap-1 bg-gradient-to-r 
-       from-darkCyan to-orange transition-all 
-       duration-500 hover:scale-110 hover:border-orange 
-       cursor-pointer hover:shadow-cyanShadow"
-        >
-          <Link
-            spy={true}
-            smooth={true}
-            duration={500}
-            offset={-120}
-            // to="contact"
-          >
-            CV Download
-          </Link>
-        </button> */}
-
       <button className=" px-4 py-2 rounded-full text-xl font-bold font-body text-white border-cyan border flex items-center gap-1 bg-gradient-to-r  from-darkCyan to-orange transition-all duration-500 hover:scale-110 hover:border-orange cursor-pointer hover:shadow-cyanShadow">
         <Link
           spy={true}
--- src/components/navbar/NavbarLinks.jsx
+++ src/components/navbar/NavbarLinks.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-scroll";
+import rahulbanikPDF from "../../../public/cvPDF/rahulbanik.pdf";
 
 const links = [
   { link: "About Me", section: "about" },
@@ -10,21 +11,44 @@ const links = [
 ];
 
 const NavbarLinks = () => {
+  const handleDownload = () => {
+    const link = document.createElement("a");
+    link.href = rahulbanikPDF;
+    link.download = "rahulbanik.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
     <ul className="flex lg:flex-row sm:flex-col gap-6 text-white font-body lg:relative sm:absolute sm:top-[120%] text-center left-[50%] -translate-x-[50%] lg:text-md sm:text-xl sm:bg-cyan/30 backdrop-blur-lg lg:bg-black sm:w-full py-4">
       {links.map((link, index) => {
         return (
           <li key={index} className="group">
-            <Link
-              spy={true}
-              smooth={true}
-              duration={500}
-              offset={-130}
-              to={link.section}
-              className="cursor-pointer text-white hover:text-cyan transition-all duration-500"
-            >
-              {link.link}
-            </Link>
+            {link?.section === "cvDownload" ? (
+              <Link
+                onClick={handleDownload}
+                spy={true}
+                smooth={true}
+                duration={500}
+                offset={-130}
+                to={link.section}
+                className="cursor-pointer text-white hover:text-cyan transition-all duration-500"
+              >
+                {link.link}
+              </Link>
+            ) : (
+              <Link
+                spy={true}
+                smooth={true}
+                duration={500}
+                offset={-130}
+                to={link.section}
+                className="cursor-pointer text-white hover:text-cyan transition-all duration-500"
+              >
+                {link.link}
+              </Link>
+            )}
             <div className="mx-auto bg-cyan w-0 group-hover:w-full h-[1px] transition-all duration-500"></div>
           </li>
         );
